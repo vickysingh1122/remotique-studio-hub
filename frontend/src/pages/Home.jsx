100be8076cc9803a0ab332ca62af0d07138b1581
vickysingh1122 <vickysingh11229900@gmail.com>
2025-06-05 13:29:26 +0530

    Add frontend file changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../components/Banner";
 import Topcompany from "../components/Topcompany";
+import OurSkills from "../components/OurSkills";
 
 function Home() {
   return (
     <>
       <Banner />
       <Topcompany />
+      <OurSkills/>
     </>
   );
 }
